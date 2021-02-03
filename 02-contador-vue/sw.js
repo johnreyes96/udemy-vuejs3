c2cbac404e385eee8c473fabec891587fde832a4
johnfreyes <jhonfer96@hotmail.com>
2021-02-03 18:35:54 -0500

    se agregan las rutas para trabajarlas offline en la caché

---
+++ 02-contador-vue/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "v1_cache_contador_app_vue"
+const urlsToCache = [
+    "./",
+    "./img/favicon.png",
+    "./img/icon32.png",
+    "./img/icon64.png",
+    "./img/icon128.png",
+    "./img/icon256.png",
+    "./img/icon512.png",
+    "./img/icon1024.png",
+    "https://unpkg.com/vue@next",
+    "./js/main.js",
+    "./js/mountApp.js",
+    "./css/style.css",
+    "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+]
