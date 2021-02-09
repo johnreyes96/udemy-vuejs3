0e9c1f43ef69b1258d072b8f3688302f81881715
johnfreyes <jhonfer96@hotmail.com>
2021-02-09 18:23:02 -0500

    Lesson five: gradient dinamic orientation

--- 05-degradados/js/main.js
+++ 05-degradados/js/main.js
@@ -2,11 +2,20 @@ const app = Vue.createApp({
     data: () => ({
         title: "Gradient Generator",
         firstColor: "#000000",
-        secondColor: "#000000"
+        secondColor: "#000000",
+        orientation: 1
     }),
     computed: {
         setColor() {
-            return `background: linear-gradient(to right, ${this.firstColor}, ${this.secondColor});`
+            if (this.orientation == 1) {
+                return `background: linear-gradient(to right, ${this.firstColor}, ${this.secondColor});`
+            } else if (this.orientation == 2) {
+                return `background: linear-gradient(to left, ${this.firstColor}, ${this.secondColor});`
+            } else if (this.orientation == 3) {
+                return `background: linear-gradient(to top, ${this.firstColor}, ${this.secondColor});`
+            } else {
+                return `background: linear-gradient(to bottom, ${this.firstColor}, ${this.secondColor});`
+            }
         }
     }
 });
