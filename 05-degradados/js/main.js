68625cca01a478bf3f2bcaa2ab6f8f3f81b2ebf8
johnfreyes <jhonfer96@hotmail.com>
2021-02-09 17:59:40 -0500

    Lesson five: generating css code lines

--- 05-degradados/js/main.js
+++ 05-degradados/js/main.js
@@ -3,5 +3,10 @@ const app = Vue.createApp({
         title: "Gradient Generator",
         firstColor: "#000000",
         secondColor: "#000000"
-    })
+    }),
+    computed: {
+        setColor() {
+            return `background: linear-gradient(to right, ${this.firstColor}, ${this.secondColor});`
+        }
+    }
 });
