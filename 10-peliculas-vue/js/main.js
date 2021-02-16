2e4bb0f720095dde5e0662bef194b94ab7d234d8
johnfreyes <jhonfer96@hotmail.com>
2021-02-16 18:13:33 -0500

    Lesson ten: get api statically

--- 10-peliculas-vue/js/main.js
+++ 10-peliculas-vue/js/main.js
@@ -1,5 +1,16 @@
 const app = Vue.createApp({
     data: () => ({
-        title: "Movies Vue"
-    })
+        title: "Movies Vue",
+        movieData: {}
+    }),
+    mounted() {
+        this.getMovie();
+    },
+    methods: {
+        async getMovie() {
+            const data = await fetch("http://www.omdbapi.com/?apikey=352d4209&t=spider+man");
+            const jsonData = await data.json();
+            this.movieData = jsonData;
+        }
+    }
 })
