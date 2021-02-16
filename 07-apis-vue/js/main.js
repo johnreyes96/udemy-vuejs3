1b482d3a53eb6eef08cab5359e16415d80f1c61c
johnfreyes <jhonfer96@hotmail.com>
2021-02-15 21:20:18 -0500

    Lesson seven: fetch in vue

--- 07-apis-vue/js/main.js
+++ 07-apis-vue/js/main.js
@@ -7,8 +7,10 @@ const app = Vue.createApp({
         this.getPost();
     },
     methods: {
-        getPost() {
-            console.log("post");
+        async getPost() {
+            const res = await fetch("https://jsonplaceholder.typicode.com/photos/10");
+            const data = await res.json();
+            console.log(data);
         }
     }
 });
