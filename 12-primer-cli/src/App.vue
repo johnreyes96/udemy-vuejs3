2c01483ca824fe658d02541dce28e33f0f0bc587
johnfreyes <jhonfer96@hotmail.com>
2021-02-19 18:56:32 -0500

    Lesson twelve: desing footer component

--- 12-primer-cli/src/App.vue
+++ 12-primer-cli/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <Header title="Portafolio GH" />
+  <div id="app">
+    <Header title="Portafolio GH" />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
---
+++ 12-primer-cli/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="footer">
+    <footer>
+        <h3>&copy; John Reyes - {{ new Date().getFullYear() }}</h3>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h3 {
+  background-color: #333;
+  padding: 1rem;
+  text-align: center;
+  color: snow;
+}
+</style>
