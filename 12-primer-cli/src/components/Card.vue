6994a432bba2df954fc6a91cd4b498b93e4743bb
johnfreyes <jhonfer96@hotmail.com>
2021-02-22 17:30:30 -0500

    Lesson twelve: agg card component

---
+++ 12-primer-cli/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cards">
+      <div class="card">
+        <h4>Name project</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio incidunt voluptatem eaque. Molestiae reiciendis eaque soluta accusamus, quia, voluptatem aliquid in assumenda maiores sunt sed earum libero qui quisquam praesentium.</p>
+        <p><strong>Author</strong></p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.card {
+    width: 250px;
+    margin: 7px;
+    border: solid 1px #eee;
+    box-shadow: 1px 1px 4px #333;
+    text-align: center;
+}
+</style>
--- 12-primer-cli/src/components/Container.vue
+++ 12-primer-cli/src/components/Container.vue
@@ -2,21 +2,44 @@
   <div id="container" class="container">
       <h2>Proyectos</h2>
       <hr>
+      <div id="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import Card from './Card';
 
+export default {
+  components: {
+    Card
+  }
 }
 </script>
 
 <style scoped>
+#cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 .container {
-    margin: 1rem;
-    padding: 7px;
-    border: solid 1px #eee;
-    box-shadow: 1px 1px 4px #333;
-    text-align: center;
+  overflow: hidden;
+  margin: 1rem;
+  padding: 7px;
+  border: solid 1px #eee;
+  box-shadow: 1px 1px 4px #333;
+  text-align: center;
 }
 </style>
