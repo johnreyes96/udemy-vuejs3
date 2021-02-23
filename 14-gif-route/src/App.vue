3dc86c9780f5da01ea836be608f93e0566931769
johnfreyes <jhonfer96@hotmail.com>
2021-02-23 17:48:59 -0500

    Lesson fourteen: create navbar

--- 14-gif-route/src/App.vue
+++ 14-gif-route/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="nav">
+    <Header />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
