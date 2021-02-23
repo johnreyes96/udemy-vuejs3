bcb628ab3eae1b453937100402a6fb937afc4008
johnfreyes <jhonfer96@hotmail.com>
2021-02-22 21:09:46 -0500

    Lesson thirteen: initial commit

---
+++ 13-custom-events/src/components/ButtonContador.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="btn-contador">
+    <button>{{ texto }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String,
+      default: "Aumentar",
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ 13-custom-events/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>Contador: {{ contador }}</h1>
+    <button-contador />
+    <button-contador texto="Disminuir" />
+  </div>
+</template>
+
+<script>
+import ButtonContador from "./ButtonContador.vue";
+export default {
+  components: { ButtonContador },
+  name: "HelloWorld",
+  props: {
+    contador: Number,
+  },
+};
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
