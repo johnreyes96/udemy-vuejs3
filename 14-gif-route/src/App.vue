be0aaf882e120229ec597cc3470229f35ef473e4
johnfreyes <jhonfer96@hotmail.com>
2021-02-23 18:09:53 -0500

    Lesson fourteen: agg first route

--- 14-gif-route/src/App.vue
+++ 14-gif-route/src/App.vue
@@ -3,8 +3,8 @@
     <Header />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <script>
--- 14-gif-route/src/components/Header.vue
+++ 14-gif-route/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <!--<a class="navbar-brand" href="/">Gif Routes</a>-->
+    <router-link class="navbar-brand" to="/">Gif Routes</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
