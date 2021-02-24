35512e4536bb74fd7f240327735ea81960e0e972
johnfreyes <jhonfer96@hotmail.com>
2021-02-24 18:04:43 -0500

    Lesson fourteen: change year calculate from data to computed method

--- 14-gif-route/src/components/Footer.vue
+++ 14-gif-route/src/components/Footer.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="footer">
       <footer class="text-center bg-dark text-white py-4">
-          <h3>&copy; {{ year }} - John Reyes</h3>
+          <h3>&copy; {{ getFullYear }} - John Reyes</h3>
       </footer>
   </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        year: new Date().getFullYear()
-    })
+    computed: {
+        getFullYear() {
+            return new Date().getFullYear();
+        }
+    }
 };
 </script>
 
