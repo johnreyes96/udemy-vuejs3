9333c94736766c7e27fd1b1f5bf87484804e8624
johnfreyes <jhonfer96@hotmail.com>
2021-02-24 17:53:20 -0500

    Lesson fourteen: Create home view

--- 14-gif-route/src/components/HelloWorld.vue
+++ 14-gif-route/src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="container mt-3">
+    <div class="card" style="width: 100%;">
+      <div class="card-body">
+        <h5 class="card-title">Gif Router</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Hecho por John Reyes</h6>
+        <p class="card-text" style="text-align: justify;">Esta aplicación es parte del curso de Vue 3 en Udemy y estamos aprendiendo a intergrar Bootstrap, a trabajar con Rutas y muchas cosas más.</p>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
