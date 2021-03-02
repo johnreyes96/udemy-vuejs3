1aac0b42e38ca15f64e38fb06098d3e0ff2186ac
johnfreyes <jhonfer96@hotmail.com>
2021-03-01 20:14:37 -0500

    Lesson fourteen: create a search component

---
+++ 14-gif-route/src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <form @submit.prevent="ejecutarPeticion">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Categoria</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <button type="submit" class="btn btn-primary">Search</button>
+    </form>
+</template>
+
+<script>
+export default {
+    methods: {
+        ejecutarPeticion() {
+            this.$emit("accion");
+        }
+    }
+}
+</script>
--- 14-gif-route/src/views/Gifs.vue
+++ 14-gif-route/src/views/Gifs.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container mt-3 border">
     <h1 class="text-center">Gifs</h1>
+    <search @accion="getGifs" />
     <hr>
     <div class="row">
       <div class="col-12">
@@ -12,8 +13,10 @@
 
 <script>
 import GifCard from '../components/GifCard.vue';
+import Search from '../components/Search.vue';
+
 export default {
-  components: { GifCard },
+  components: { GifCard, Search },
   data: () => ({
     gifs: {}
   }),
