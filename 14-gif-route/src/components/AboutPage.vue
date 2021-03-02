3123cb0e2c0a083c56cac6e9151b88c1465bffc5
johnfreyes <jhonfer96@hotmail.com>
2021-03-01 21:04:27 -0500

    Lesson fourteen: create about view

---
+++ 14-gif-route/src/components/AboutPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mt-3">
+    <div class="card" style="width: 100%;">
+      <div class="card-body">
+        <h5 class="card-title">About</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Hecho por John Reyes</h6>
+        <p class="card-text" style="text-align: justify;">Esta aplicación utiliza la API de
+          <a href="http://www.giphy.com">giphy.com</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- 14-gif-route/src/views/About.vue
+++ 14-gif-route/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <about-page />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import AboutPage from '@/components/AboutPage.vue';
+
+export default {
+  name: 'About',
+  components: {
+    AboutPage
+  }
+};
+</script>
