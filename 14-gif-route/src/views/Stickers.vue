e2fd8cd224a18893ab515500f2762207399f430d
johnfreyes <jhonfer96@hotmail.com>
2021-03-01 20:54:47 -0500

    Lesson fourteen: create stickers view

--- 14-gif-route/src/views/Stickers.vue
+++ 14-gif-route/src/views/Stickers.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>Vista de Stickers</h1>
+  <div class="container mt-3 border">
+    <h1 class="text-center">Stickers</h1>
+    <search @accion="getStickers" />
+    <hr>
+    <loading v-if="load" />
+    <div class="row">
+      <div class="col-12 col-lg-3" v-for="sticker in stickers" :key="sticker.id">
+        <sticker-card :data="sticker" class="m-3 w-75" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Loading from '../components/Loading.vue';
+import Search from '../components/Search.vue';
+import StickerCard from '../components/StickerCard.vue';
 
-}
+export default {
+  components: { Search, Loading, StickerCard },
+  data: () => ({
+    stickers: {},
+    load: false
+  }),
+  created() {
+    this.getStickers();
+  },
+  methods: {
+    async getStickers(search = "goku") {
+      const key = "Xn9bsf30qq5WnNfS1BB5Fn7VwE2fDJdl";
+      this.load = true;
+      const { data } = await this.axios.get(`https://api.giphy.com/v1/stickers/search?q=${search}&api_key=${key}`);
+      this.stickers = data.data;
+      this.load = false;
+    }
+  }
+};
 </script>
