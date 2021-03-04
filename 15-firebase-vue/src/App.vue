c95465a1ff5883a035f6bf9c4c730a8895eb5e64
johnfreyes <jhonfer96@hotmail.com>
2021-03-04 18:33:29 -0500

    Lesson fifteen: install materialize and create navbar component

--- 15-firebase-vue/src/App.vue
+++ 15-firebase-vue/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="nav">
+    <navbar />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: { Navbar }
+}
+</script>
