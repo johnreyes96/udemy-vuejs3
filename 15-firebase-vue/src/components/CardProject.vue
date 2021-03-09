3760313fa431cb60655b32a54b8fcfd124e59ae4
johnfreyes <jhonfer96@hotmail.com>
2021-03-09 17:41:51 -0500

    Lesson fifteen: modularizing cards

---
+++ 15-firebase-vue/src/components/CardProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card horizontal">
+    <div class="card-stacked">
+      <div class="card-content">
+        <span class="card-title">Proyecto 1</span>
+          <p>Descripción de mi proyecto</p>
+      </div>
+      <div class="card-action">
+        <div class="row">
+          <a class="col s6 waves-effect waves-light btn amber darken-3">
+            <i class="material-icons">edit</i>
+          </a>
+          <a class="col s6 waves-effect waves-light btn red darken-3">
+            <i class="material-icons">delete</i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- 15-firebase-vue/src/components/Contenedor.vue
+++ 15-firebase-vue/src/components/Contenedor.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="row">
     <div class="col s12 m7">
-      <div class="card horizontal">
-        <div class="card-stacked">
-          <div class="card-content">
-            <span class="card-title">Proyecto 1</span>
-            <p>I am a very simple card. I am good at containing small bits of information.</p>
-          </div>
-          <div class="card-action">
-            <div class="row">
-              <a class="col s6 waves-effect waves-light btn amber darken-3">
-                <i class="material-icons">edit</i>
-              </a>
-              <a class="col s6 waves-effect waves-light btn red darken-3">
-                <i class="material-icons">delete</i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <card-project />
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import CardProject from './CardProject.vue';
 
-}
+export default {
+  components: { CardProject },
+
+};
 </script>
