cb090d31ffe7965523f7973d10ea54e3d9ba0bd7
johnfreyes <jhonfer96@hotmail.com>
2021-03-09 17:28:02 -0500

    Lesson fifteen: customizing tasks

--- 15-firebase-vue/src/components/Contenedor.vue
+++ 15-firebase-vue/src/components/Contenedor.vue
@@ -4,10 +4,14 @@
       <div class="card horizontal">
         <div class="card-stacked">
           <div class="card-content">
+            <span class="card-title">Proyecto 1</span>
             <p>I am a very simple card. I am good at containing small bits of information.</p>
           </div>
           <div class="card-action">
-            <a href="#">This is a link</a>
+            <div class="row">
+              <a class="col s6 waves-effect waves-light btn">Editar</a>
+              <a class="col s6 waves-effect waves-light btn">Eliminar</a>
+            </div>
           </div>
         </div>
       </div>
