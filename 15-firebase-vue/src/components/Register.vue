618259300b6988a87b4850124d547acfdb81e81b
johnfreyes <jhonfer96@hotmail.com>
2021-03-12 17:36:31 -0500

    Lesson fifteen: agg login view

---
+++ 15-firebase-vue/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <form @submit.prevent="" class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Confirm Password</label>
+        </div>
+      </div>
+      <button type="submit" class="waves-effect waves-light btn">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
--- 15-firebase-vue/src/views/Home.vue
+++ 15-firebase-vue/src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
-    <h1>Home </h1>
+    <register />
   </div>
 </template>
 
 <script>
+import Register from '../components/Register.vue';
+
 export default {
+  components: { Register },
   name: 'Home'
-}
+};
 </script>
