396a0ea91fd5c8fcc9f752da00f3aa89211a52cb
johnfreyes <jhonfer96@hotmail.com>
2021-03-15 21:43:39 -0500

    Lesson fifteen: protected routes

--- 15-firebase-vue/src/router/index.js
+++ 15-firebase-vue/src/router/index.js
@@ -21,7 +21,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (proyectos.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "proyectos" */ '../views/Proyectos.vue')
+    component: () => 
+      import(/* webpackChunkName: "proyectos" */ '../views/Proyectos.vue'),
+      meta: { protect: true }
   },
   {
     path: '/reg-project',
@@ -29,7 +31,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (reg-project.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "reg-project" */ '../views/Created.vue')
+    component: () =>
+      import(/* webpackChunkName: "reg-project" */ '../views/Created.vue'),
+      meta: { protect: true }
   },
   {
     path: '/edit-project/:id',
@@ -37,13 +41,27 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (edit-project.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "edit-project" */ '../views/Edit.vue')
+    component: () =>
+      import(/* webpackChunkName: "edit-project" */ '../views/Edit.vue'),
+      meta: { protect: true }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.protect) {
+    if (localStorage.getItem("user")) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
