81c47bd39e1d675c89f88ee091dabff9a3c387fb
johnfreyes <jhonfer96@hotmail.com>
2021-03-15 21:31:23 -0500

    Lesson fifteen: log Out

--- 15-firebase-vue/src/components/Navbar.vue
+++ 15-firebase-vue/src/components/Navbar.vue
@@ -6,13 +6,16 @@
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
             <li>
-                <router-link to="/">Register</router-link>
+                <router-link v-if="!sesion" to="/">Register</router-link>
             </li>
             <li>
-                <router-link to="/login">LogIn</router-link>
+                <router-link v-if="!sesion" to="/login">LogIn</router-link>
             </li>
             <li>
-                <router-link to="/proyectos" class="waves-effect waves-light btn red lighten">Proyectos</router-link>
+                <router-link v-if="sesion" to="/proyectos" class="waves-effect waves-light btn red lighten">Proyectos</router-link>
+            </li>
+            <li>
+                <router-link v-if="sesion" to="/" @click="cerrarSesion" class="waves-effect waves-light btn red lighten">Cerrar Sesión</router-link>
             </li>
         </ul>
         </div>
@@ -20,13 +23,16 @@
 
     <ul class="sidenav" id="mobile-demo">
         <li>
-            <router-link to="/">Register</router-link>
+            <router-link v-if="!sesion" to="/">Register</router-link>
         </li>
         <li>
-            <router-link to="/login">LogIn</router-link>
+            <router-link v-if="!sesion" to="/login">LogIn</router-link>
         </li>
         <li>
-            <router-link to="/proyectos">Proyectos</router-link>
+            <router-link v-if="sesion" to="/proyectos">Proyectos</router-link>
+        </li>
+        <li>
+            <router-link v-if="sesion" to="/" @click="cerrarSesion" class="waves-effect waves-light btn red lighten">Cerrar Sesión</router-link>
         </li>
     </ul>
   </div>
@@ -36,6 +42,20 @@
 export default {
     mounted() {
         M.AutoInit();
+    },
+    methods: {
+        cerrarSesion() {
+            localStorage.removeItem("user");
+        }
+    },
+    computed: {
+        sesion() {
+            const user = localStorage.getItem("user");
+            if (user) {
+                return true;
+            }
+            return false;
+        }
     }
 }
 </script>
