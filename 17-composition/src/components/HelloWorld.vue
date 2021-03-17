1de491d1acccf6160607c020d82befd9f1b148d9
johnfreyes <jhonfer96@hotmail.com>
2021-03-17 17:49:57 -0500

    Lesson seventeen: reactive references

--- 17-composition/src/components/HelloWorld.vue
+++ 17-composition/src/components/HelloWorld.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <hr>
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: 'HelloWorld',
   setup() {
-    const title = "Composition";
-    return { title };
+    const title = "Aprendiendo Composition";
+    const contador = ref(0);
+    const aumentar = () => contador.value++;
+    const disminuir = () => contador.value--;
+    
+    return { title, contador, aumentar, disminuir };
   }
-}
+};
 </script>
